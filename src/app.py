2c391ad87932799ef57caa637553024479c0b817
Jake Torres <jake@torresengineering.dev>
2022-10-04 15:18:09 -0600

    Removed messaging directory and put Twilio messaging logic in main app module. Successfully triggers Twilio phone number to send back a response from the app.

--- src/app.py
+++ src/app.py
@@ -1,12 +1,70 @@
 """Driver Flask application."""
-from flask import Flask
+import os
+import sys
 
-# App initialization
+from dotenv import load_dotenv
+from flask import Flask, request
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+# Load environment variables
+load_dotenv()
+
+# Initialize and configure app
 app = Flask(__name__)
+app.config.from_mapping(
+    BASE_URL="http://localhost:5000",
+    ENV="development",
+    USE_NGROK=os.getenv('USE_NGROK', False) == "True"
+)
+
+# Import and initialize pyngrok for HTTP tunneling
+# only if in development environment and if USE_NGROK is True
+if app.config.get('ENV') == "development" and app.config['USE_NGROK']:
+    from pyngrok import ngrok
+
+    # Assign port number from command line arg or default
+    port = sys.argv(
+        sys.argv.index('--port') + 1
+    ) if '--port' in sys.argv else 5000
+
+    # Get ngrok public URL
+    public_url = ngrok.connect(port).public_url
+
+    print(f" * ngrok HTTP tunnel started at {public_url}"
+                            f" from http://127.0.0.1:{port}")
+
+    # Change base URL to the new ngrok URL
+    app.config['BASE_URL'] = public_url
+
+# Connect to Twilio API
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+outgoing_phone = os.getenv('TWILIO_PHONE_NUMBER')
+client = Client(account_sid, auth_token)
+
+"""Route definitions"""
+
+# Test route
+@app.route('/')
+def index():
+    return "Yay it works!"
 
 
-# Create app route sms with methods GET and POST
-# Define a function: get_availability
+@app.route('/sms', methods=['GET', 'POST'])
+def get_availability():
+    if request.method == 'POST':
+        response = MessagingResponse()
+        response.message(
+            to="+17205821400",
+            from_=outgoing_phone,
+            body="This is a test of the emergency alert system."
+        )
+
+    return str(response)
+
+
 # Store the sender's number in a variable
 # Access Calendly calendar and store the last 3 empty time slots in a variable
 # Send the time slots to the sender in an SMS message
